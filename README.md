69825bd596840c1afb64e95cc4bf60bc3c346e23
aharri64 <haveacompleteday@gmail.com>
2021-02-04 13:07:41 -0700

    connected to swapi

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ console.log(process.env.API_KEY)
 ```
 - process.env is the environment of our computer
 - protect you API because people got ustilize it
+
+## We're going to use the Open Movie Database
+
+Docs: http://www.omdbapi.com/
+
+Docs: Axios https://github.com/axios/axios
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 // console.log(process.env.API_KEY)
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
 
@@ -20,5 +21,43 @@ app.get('/', (req, res) => {
     res.send('hello')
 })
 
-const PORT = process.env.PORT || 8000
+app.get('/omdb', (req, res) => {
+    const qs = {
+        params: {
+            s: 'star wars',
+            apikey: process.env.API_KEY
+        }
+    }
+    // http://www.omdbapi.com/?s=star+wars&apikey=OUR+key
+    axios.get('http://www.omdbapi.com', qs)
+        .then((response) => {
+            console.log(response.data)
+        })
+})
+
+// with the swapi API
+app.get('/swapi/search', (req, res) => {
+    res.render('search')
+})
+
+app.get('/swapi/show', (req, res) => {
+    console.log('made it here')
+    // console.log('param', req.params) // did not need
+    console.log('query', req.query) // basic api call to get a person
+    // console.log('body', req.body) //did not need
+    axios.get(`https://swapi.dev/api/people/${req.query.personID}`)
+        .then((response) => {
+            //response.data os where our data lives
+            console.log(response.data)
+            //make a person object
+            const person = {
+                name: response.data.name,
+                birth: response.data['birth_year'],
+                home: response.data.homeworld
+            }
+            res.render('show', person)
+        })
+})
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`listening on ${PORT}🛳`))
